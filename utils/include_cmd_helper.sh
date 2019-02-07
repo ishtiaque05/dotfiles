37580443e5245988324168bbabc1b9094d1d6a2b
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-02-07 23:26:47 +0600

    fix syntax error

--- utils/include_cmd_helper.sh
+++ utils/include_cmd_helper.sh
@@ -3,17 +3,17 @@
 URL=https://raw.githubusercontent.com/ishtiaque05/dotfiles/master/helpers/.cmd_helper
 if [ -n "$ZSH_VERSION" ]; then
    # assume Zsh
-   if grep -q .cmd_helper "$HOME/.zshrc"; then
+   if [ grep -q .cmd_helper "$HOME/.zshrc"];then
       echo "Command helper already included"
    else
      wget_output=$(wget -q "$URL" -O $HOME/.cmd_helper)
      if [ $? -ne 0 ]; then
        echo "source $HOME/.cmd_helper" >> $HOME/.zshrc
-     fi 
+     fi
    fi
 elif [ -n "$BASH_VERSION" ]; then
    # assume Bash
-else
    # asume something else
+   echo "Bash file"
 fi
 
