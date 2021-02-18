779207985218cc568a5b7bc2b160af11f8ecedd3
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2021-02-17 18:21:21 -0800

    Create json_pretty.sh

---
+++ utils/json_pretty.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+# Usage: cat example.json | json_pretty 
+# Pretty print json in terminal
+grep -Eo '"[^"]*" *(: *([0-9]*|"[^"]*")[^{}\["]*|,)?|[^"\]\[\}\{]*|\{|\},?|\[|\],?|[0-9 ]*,?' | awk '{if ($0 ~ /^[}\]]/ ) offset-=4; printf "%*c%s\n", offset, " ", $0; if ($0 ~ /^[{\[]/) offset+=4}'
