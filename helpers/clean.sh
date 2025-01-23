7d440729810474f513c2ce4ac44a7d18cfa8d705
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2025-01-23 00:13:43 -0800

    added dotfiles zshrc, zoxide, eza, wezterm, powerlevel10k

--- helpers/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-function clean {
-  sudo apt-get autoremove -y --force-yes
-  sudo apt-get autoclean -y --force-yes
-  sudo apt-get install -f -y --force-yes
-}
--- helpers/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-function update {
-  sudo apt-get update -y;
-}
